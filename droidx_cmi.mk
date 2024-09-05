79eb0e8657dd3bbe1d596951f5711e05baf9c99d
alongfei <110464232+alongfei@users.noreply.github.com>
2024-09-05 09:18:40 +0800

    Update droidx_cmi.mk

--- droidx_cmi.mk
+++ droidx_cmi.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The DroidX-UI Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -9,12 +9,22 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/droidx/config/common_full_phone.mk)
 
 # Inherit from cmi device
 $(call inherit-product, device/xiaomi/cmi/device.mk)
 
-PRODUCT_NAME := lineage_cmi
+# Device props
+TARGET_SUPPORTS_BLUR := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Charging Animation
+TARGET_USE_PIXEL_CHARGER := true
+
+Maintainer := Charon
+
+PRODUCT_NAME := droidx_cmi
 PRODUCT_DEVICE := cmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
