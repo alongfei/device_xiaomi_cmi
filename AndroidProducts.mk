0c9e75ab8885449db2ea05761136289a4c76b0db
alongfei <110464232+alongfei@users.noreply.github.com>
2024-09-05 09:13:47 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,8 +1,13 @@
 #
-# Copyright (C) 2021 The LineageOS Project
+# Copyright (C) 2021 The DroidX-UI Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_cmi.mk
+    $(LOCAL_DIR)/droidx_cmi.mk
+
+COMMON_LUNCH_CHOICES := \
+    droidx_cmi-ap2a-user \
+    droidx_cmi-ap2a-userdebug \
+    droidx_cmi-ap2a-eng
